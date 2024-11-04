9e793c45df70fe9a0929dd7f3236248d6a9e7404
santosh <santoshkumar.btpl@gmail.com>
2024-11-04 12:23:07 +0530

    [IMP] lims_inventory: - Separate settings for "lims inventory" instead of "purchase" - Added company-specific icon at settings to differentiate global and company level setting - Added user-wise hide/show fields

--- models/res_company.py
+++ models/res_company.py
@@ -5,13 +5,13 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     # 7span customization
-    show_contract_name = fields.Boolean(string="Show Contract Name")
-    show_vendor_id = fields.Boolean(string="Show Vendor")
-    show_customer_id = fields.Boolean(string="Show Customer")
-    show_start_date = fields.Boolean(string="Show Start Date")
-    show_end_date = fields.Boolean(string="Show End Date")
-    show_currency_id = fields.Boolean(string="Show Currency")
-    show_flag = fields.Boolean(string="Show Flag")
-    show_product_lines = fields.Boolean(string="Show Product Lines")
-    show_create_date = fields.Boolean(string="Show Created Date")
-    show_state = fields.Boolean(string="Show Status")
+    show_contract_name = fields.Boolean(default=True, string="Show Contract Name")
+    show_vendor_id = fields.Boolean(default=True, string="Show Vendor")
+    show_customer_id = fields.Boolean(default=True, string="Show Customer")
+    show_start_date = fields.Boolean(default=True, string="Show Start Date")
+    show_end_date = fields.Boolean(default=True, string="Show End Date")
+    show_currency_id = fields.Boolean(default=True, string="Show Currency")
+    show_flag = fields.Boolean(default=True, string="Show Flag")
+    show_product_lines = fields.Boolean(default=True, string="Show Product Lines")
+    show_create_date = fields.Boolean(default=True, string="Show Created Date")
+    show_state = fields.Boolean(default=True, string="Show Status")
